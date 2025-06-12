8b67ad9ef995863f05c4ca7bcb0ab3c15ff1bc3b
Wattanapongphan <wattanapongphan@gmail.com>
2025-06-12 15:07:30 +0700

    Add Login and Navigation guard

--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -1,28 +1,52 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "@/plugins/stores/auth";
+// import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // },
+
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },        
-    
-    {
-      path: '/',
-      component: () => import('@/layouts/BlankLayout.vue'),
+      path: "",
+      component: () => import("@/layouts/BlankLayout.vue"),
+      meta: { requireAuth: false },
       children: [
         {
-              path: 'login',
-              name: 'Login',
-              component: () => import('@/views/LoginView.vue'),
+          path: "/login",
+          name: "Login",
+          component: () => import("@/views/LoginView.vue"),
         },
-      ]
+      ],
     },
 
+    {
+      path: "/",
+      component: () => import("@/layouts/DefaultLayout.vue"),
+      meta: { requireAuth: true },
+      children: [
+        {
+          path: "home",
+          name: "home",
+          component: () => import("@/views/HomeView.vue"),
+        },
+      ],
+    },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore()
+
+  if (to.meta.requireAuth && !auth.isLoggedIn) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
 })
 
-export default router
+export default router;
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,21 +4,43 @@
             <v-card-title class="text-h5 text-center">เข้าสู่ระบบ</v-card-title>
             <v-card-text>
                 <v-form>
-                    <v-text-field label="อีเมล" type="email" required />
+                    <v-text-field v-model="username" label="Username" type="text" required />
 
-                    <v-text-field label="รหัสผ่าน" type="password" required />
-
-                    <v-btn type="submit" color="primary" block class="mt-4">
-                        เข้าสู่ระบบ
-                    </v-btn>
+                    <v-text-field v-model="password" label="Password" type="password" required />
                 </v-form>
             </v-card-text>
+            <v-card-actions>
+                <v-btn type="submit" block class="mt-4" style="background-color: black; color: white;" @click="handleLogin">
+                    เข้าสู่ระบบ
+                </v-btn>
+            </v-card-actions>
         </v-card>
     </v-container>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/plugins/stores/auth'
 
+const auth = useAuthStore()
+
+const router = useRouter()
+
+const username = ref('')
+const password = ref('')
+
+function handleLogin() {
+  if ( username.value && password.value) {
+    const fakeToken = 'abcd1234'
+    auth.login(fakeToken)
+    router.push('/home')
+}else{
+    alert('กรุณากรอกข้อมูลให้ครบถ้วน')
+  }
+}
 </script>
 
-<style scoped></style>
+<style lang="scss">
+
+</style>
